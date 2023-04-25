c59d04dfe691c34490ab70d9683e238b63daa0b3
salar <sgha0017@student.monash.edu>
2023-04-26 07:44:40 +1000

    Created the base version of Grossmesser

---
+++ src/game/items/weapons/Grossmesser.java
@@ -0,0 +1,11 @@
+package game.items.weapons;
+
+import edu.monash.fit2099.engine.weapons.WeaponItem;
+
+public class Grossmesser extends WeaponItem {
+
+    public Grossmesser() {
+        super("Grossmesser", '?', 115, "Slash", 85);
+    }
+
+}
