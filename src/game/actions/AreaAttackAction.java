19bdf196018ac1692d8ae378bd77c0348c7678cd
salar <sgha0017@student.monash.edu>
2023-04-26 21:14:52 +1000

    created and implemented AreaAttackAction modified Grossmesser to work with AreaAttackAction

---
+++ src/game/actions/AreaAttackAction.java
@@ -0,0 +1,54 @@
+package game.actions;
+
+import edu.monash.fit2099.engine.actions.Action;
+import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.positions.GameMap;
+import edu.monash.fit2099.engine.weapons.Weapon;
+import edu.monash.fit2099.engine.weapons.WeaponItem;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class AreaAttackAction extends Action {
+
+    ArrayList<Actor> targetList;
+    /**
+     * Random number generator
+     */
+    private Random rand = new Random();
+
+    /**
+     * Weapon used for the attack
+     */
+    private Weapon weapon;
+
+    public AreaAttackAction(ArrayList<Actor> targetList, WeaponItem weapon) {
+        this.targetList = targetList;
+        this.weapon = weapon;
+    }
+
+    @Override
+    public String execute(Actor actor, GameMap map) {
+        String result = "";
+        for(Actor target: targetList){
+            if (!(rand.nextInt(100) <= weapon.chanceToHit())) {
+               result = result + actor + " misses " + target + ".\n";
+            }
+            else{
+                int damage = weapon.damage();
+                result = result + actor + " " + weapon.verb() + " " + target + " for " + damage + " damage.";
+                target.hurt(damage);
+                if (!target.isConscious()) {
+                    result += new DeathAction(actor).execute(target, map) + "\n";
+                }
+            }
+        }
+        return result;
+    }
+
+
+    @Override
+    public String menuDescription(Actor actor) {
+        return actor + " area attacks all the actors surrounding";
+    }
+}
--- src/game/items/weapons/Grossmesser.java
+++ src/game/items/weapons/Grossmesser.java
@@ -1,6 +1,12 @@
 package game.items.weapons;
 
+import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.positions.Exit;
+import edu.monash.fit2099.engine.positions.Location;
 import edu.monash.fit2099.engine.weapons.WeaponItem;
+import game.actions.AreaAttackAction;
+
+import java.util.ArrayList;
 
 /**
  * A curved sword that can be used to attack the enemy.
@@ -12,6 +18,8 @@ import edu.monash.fit2099.engine.weapons.WeaponItem;
  */
 public class Grossmesser extends WeaponItem {
 
+   private boolean isattakActionAvailable = false;
+
     /**
      * Constructor
      */
@@ -19,4 +27,33 @@ public class Grossmesser extends WeaponItem {
         super("Grossmesser", '?', 115, "Slashes", 85);
     }
 
+    public void removeAreaAction(){
+        for(int i = 0; i < this.getAllowableActions().size(); i++){
+            if(getAllowableActions().get(i).getClass().equals(AreaAttackAction.class)){
+                this.removeAction(getAllowableActions().get(i));
+                isattakActionAvailable = false;
+            }
+        }
+    }
+     @Override
+    public void tick(Location currentLocation, Actor actor) {
+        boolean isActorAround = false;
+         ArrayList<Actor> targetlist = new ArrayList<>();
+        for (Exit exit : currentLocation.getExits()) {
+            Location destination = exit.getDestination();
+
+            if (destination.containsAnActor()) {
+                isActorAround = true;
+                targetlist.add(destination.getActor());
+            }
+        }
+
+        removeAreaAction();
+
+        if(isActorAround){
+            AreaAttackAction areaAttackAction = new AreaAttackAction(targetlist, this);
+            this.addAction(areaAttackAction);
+            this.isattakActionAvailable = true;
+        }
+    }
 }
