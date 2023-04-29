1306efc6e9c3f8453a582ea9ad98599814132a5a
Ibrahem <ikha0010@student.monash.edu>
2023-04-30 02:21:47 +1000

    > added the brand new, new and improved AOEAction, fulled by my 2AM hardwork it can perform an attack action in all 8 directions( or all available exits of a location) > the attack action can now do 2 new things    -allow actors to be insta killed( more can be added in switch case)    -allow actors to be replaced on death instead of erased (also has a switch case) > added a new attack behaviour that allowes enemies to have the sufficent intelligence to perform self defence (or annihilate you, depends on your skill) > The Enemy class now bears all the Play turn and allowable actions functionality the rest of the enmies now no longer have this > giant crab now spawns with a SLAM weapon > speaking of the SLAM it is first of all new and it also has a special ability that calles on the AOE attack >pile of bones has an intrinsic weapon that does 0 damage, kinda like a magikarp (not that we'll ever use it) thats all for now, some of these are still untested so more on that will come later

--- src/game/actors/enemies/LoneWolf.java
+++ src/game/actors/enemies/LoneWolf.java
@@ -1,19 +1,7 @@
 package game.actors.enemies;
 
-import edu.monash.fit2099.engine.actions.Action;
-import edu.monash.fit2099.engine.actions.ActionList;
-import edu.monash.fit2099.engine.actions.DoNothingAction;
-import edu.monash.fit2099.engine.actors.Actor;
-import edu.monash.fit2099.engine.displays.Display;
-import edu.monash.fit2099.engine.positions.GameMap;
 import edu.monash.fit2099.engine.weapons.IntrinsicWeapon;
-import game.behaviours.Behaviour;
-import game.Status;
 import game.behaviours.WanderBehaviour;
-import game.actions.AttackAction;
-
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * BEHOLD, DOG!
@@ -31,25 +19,6 @@ public class LoneWolf extends Enemy {
         this.behaviours.put(999, new WanderBehaviour());
     }
 
-
-    /**
-     * The lone wolf can be attacked by any actor that has the HOSTILE_TO_ENEMY capability
-     *
-     * @param otherActor the Actor that might be performing attack
-     * @param direction  String representing the direction of the other Actor
-     * @param map        current GameMap
-     * @return
-     */
-    @Override
-    public ActionList allowableActions(Actor otherActor, String direction, GameMap map) {
-        ActionList actions = new ActionList();
-        if(otherActor.hasCapability(Status.HOSTILE_TO_ENEMY)){
-            actions.add(new AttackAction(this, direction));
-            // HINT 1: The AttackAction above allows you to attack the enemy with your intrinsic weapon.
-            // HINT 1: How would you attack the enemy with a weapon?
-        }
-        return actions;
-    }
 
 
     @Override
--- src/game/actors/enemies/PileOfBones.java
+++ src/game/actors/enemies/PileOfBones.java
@@ -3,23 +3,18 @@ package game.actors.enemies;
 import edu.monash.fit2099.engine.actions.Action;
 import edu.monash.fit2099.engine.actions.ActionList;
 import edu.monash.fit2099.engine.actions.DoNothingAction;
-import edu.monash.fit2099.engine.actors.Actor;
 import edu.monash.fit2099.engine.displays.Display;
 import edu.monash.fit2099.engine.positions.GameMap;
-import edu.monash.fit2099.engine.weapons.WeaponItem;
-import game.Status;
-import game.actions.AttackAction;
+import edu.monash.fit2099.engine.weapons.IntrinsicWeapon;
 import game.actions.ReplaceAction;
-import game.behaviours.Behaviour;
 import game.items.weapons.Grossmesser;
 
-import java.util.ArrayList;
-
-public class PileOfBones extends Enemy{
+public class PileOfBones extends Enemy {
 
     private int turnCount = 3;
-    public PileOfBones(){
-        super("Pile Of Bones", 'x',1);
+
+    public PileOfBones() {
+        super("Pile Of Bones", 'x', 1);
         // It carries a grossmesser so it can drop it upon death
         this.addWeaponToInventory(new Grossmesser());
 
@@ -33,14 +28,14 @@ public class PileOfBones extends Enemy{
      * @param map        the map containing the Actor
      * @param display    the I/O object to which messages may be written
      * @return the valid action that can be performed in that iteration or null if no valid action is found
-     *
+     * <p>
      * The Pile of bones simply does nothing every turn while counting how many turns it has gone through
      * thus it overrides the Enemy parent method
      */
     @Override
     public Action playTurn(ActionList actions, Action lastAction, GameMap map, Display display) {
         //check if out of turns
-        if (turnCount == 0){
+        if (turnCount == 0) {
             //if out of turns despawn the pile of bones and summon a HSS
             return new ReplaceAction(new HeavySkeletalSwordsman());
         } else {
@@ -48,23 +43,8 @@ public class PileOfBones extends Enemy{
             return new DoNothingAction();
         }
     }
-
-    /**
-     * Pile of Bones cannot attack the enemy and can only be attacked
-     *
-     * @param otherActor the Actor that might be performing attack
-     * @param direction  String representing the direction of the other Actor
-     * @param map        current GameMap
-     * @return
-     */
     @Override
-    public ActionList allowableActions(Actor otherActor, String direction, GameMap map) {
-        ActionList actions = new ActionList();
-        if(otherActor.hasCapability(Status.HOSTILE_TO_ENEMY)){
-            actions.add(new AttackAction(this, direction));
-            // HINT 1: The AttackAction above allows you to attack the enemy with your intrinsic weapon.
-            // HINT 1: How would you attack the enemy with a weapon?
-        }
-        return actions;
+    public IntrinsicWeapon getIntrinsicWeapon() {
+        return new IntrinsicWeapon(0, "chatters", 100);
     }
 }
