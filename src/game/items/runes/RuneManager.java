f9698a47c9bfd3143b8b60c2c56aa4d978e3277d
Ibrahem <ikha0010@student.monash.edu>
2023-04-30 18:07:10 +1000

    Added rune Manager and rune pile >rune manager is a class similar to ResetManager(only a single instance of it can exist) > it has 3 methods so far:    -addRunes() adds runes to wallet    -isValidSubtraction() checks to see if a subtraction is valid    -subtractRunes() uses isValidSubtraction() and subtracts from the wallet if true

---
+++ src/game/items/runes/RuneManager.java
@@ -0,0 +1,60 @@
+package game.items.runes;
+
+public class RuneManager {
+    private int runes;
+    private static RuneManager instance;
+
+    private RuneManager() {this.runes = 0;}
+
+    public static RuneManager getInstance(){
+        if (RuneManager.instance == null){
+            RuneManager.instance = new RuneManager();
+        }
+        return RuneManager.instance;
+    }
+
+
+    /**
+     * Adds runes to wallet
+     *
+     * @param runes Runes that are to be added into the wallet
+     * @return Balance   returns the balance after adding the runes
+     */
+    public String addRunes(int runes){
+        this.runes += runes;
+        return Integer.toString( this.runes );
+    }
+
+    /**
+     * checks if you can subtract runes
+     * will return a false if the subtraction goes under 0
+     * meaning you cant spend money you dont have
+     *
+     * @param runes Runes that are to be removed from the wallet
+     * @return bool   that specifies weither the subtraction is greater or equal to 0
+     */
+    public boolean isValidSubtraction(int runes){
+        if (this.runes - runes < 0){
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    /**
+     * subtracts runes or tells you you dont have enough balance
+     *
+     * @param runes Runes that are to be removed from the wallet
+     * @return balance   returns the balance after adding the runes
+     */
+    public String subtractRunes(int runes){
+        if (isValidSubtraction(runes)){
+            this.runes -= runes;
+            return Integer.toString( this.runes );
+        }else{
+            return "You dont have enough runes!";
+        }
+    }
+
+
+}
