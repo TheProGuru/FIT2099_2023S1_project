59e14975eb19feecbcc86712576268ea1bad24e3
salar <sgha0017@student.monash.edu>
2023-05-03 20:58:19 +1000

    BuyAction and SellAction now work with Runes(RuneManager specifically)

--- src/game/actions/BuyAction.java
+++ src/game/actions/BuyAction.java
@@ -2,8 +2,12 @@ package game.actions;
 
 import edu.monash.fit2099.engine.actions.Action;
 import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.items.Item;
 import edu.monash.fit2099.engine.positions.GameMap;
+import edu.monash.fit2099.engine.weapons.Weapon;
+import edu.monash.fit2099.engine.weapons.WeaponItem;
 import game.actors.Player;
+import game.items.runes.RuneManager;
 
 public class BuyAction extends Action {
 
@@ -17,13 +21,29 @@ public class BuyAction extends Action {
         this.player = player;
         this.item = item;
     }
+    public void addBuyable(Player player, Buyable buyable){
+        player.addValuable(buyable);
+        if (buyable instanceof Weapon){
+            player.addWeaponToInventory((WeaponItem) buyable);
+        }
+        else{
+            player.addItemToInventory((Item)buyable);
+        }
+    }
 
     @Override
     public String execute(Actor actor, GameMap map) {
 
-        String result = actor + " bought " + item + " from " + merchant + " for " + item.getBuyPrice() + " runes";
-        item.handlePurchase(player);
-        return result;
+
+        RuneManager rm = RuneManager.getInstance();
+        if(rm.isValidSubtraction(item.getBuyPrice())){
+            String result = actor + " bought " + item + " from " + merchant + " for " + item.getBuyPrice() + " runes";
+            addBuyable(player, item);
+            return result;
+        }
+        else {
+            return actor + "doesn't have enough runes to buy " + item;
+        }
     }
 
     @Override
--- src/game/actions/SellAction.java
+++ src/game/actions/SellAction.java
@@ -2,8 +2,11 @@ package game.actions;
 
 import edu.monash.fit2099.engine.actions.Action;
 import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.items.Item;
 import edu.monash.fit2099.engine.positions.GameMap;
+import edu.monash.fit2099.engine.weapons.WeaponItem;
 import game.actors.Player;
+import game.items.runes.RuneManager;
 
 public class SellAction extends Action {
 
@@ -18,11 +21,27 @@ public class SellAction extends Action {
         this.item = item;
     }
 
+    public void removeBuyable(Player player, Buyable buyable){
+        player.removeValuable(buyable);
+        for(WeaponItem weapon: player.getWeaponInventory()){
+            if ( buyable == weapon){
+                player.removeWeaponFromInventory(weapon);
+            }
+        }
+        for(Item item: player.getItemInventory()){
+            if ( buyable == item){
+                player.removeItemFromInventory(item);
+            }
+        }
+    }
+
     @Override
     public String execute(Actor actor, GameMap map) {
 
+        RuneManager rm = RuneManager.getInstance();
+        rm.addRunes(item.getSellPrice());
         String result = actor + " sold " + item + " to " + merchant + " for " + item.getSellPrice()+ " runes";
-        item.handleSale(player);
+        removeBuyable(player, item);
         return result;
     }
 
