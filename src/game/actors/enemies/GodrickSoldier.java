d49d7250c4b6013187297e36894ca502e97f89d2
Ibrahem <ikha0010@student.monash.edu>
2023-05-11 19:17:37 +1000

    > godrick soldier now uses crossbow

--- src/game/actors/enemies/GodrickSoldier.java
+++ src/game/actors/enemies/GodrickSoldier.java
@@ -1,15 +1,22 @@
 package game.actors.enemies;
 
 import edu.monash.fit2099.engine.weapons.IntrinsicWeapon;
+import game.behaviours.AttackBehaviour;
+import game.behaviours.RangeBehaviour;
+import game.items.weapons.HeavyCrossbow;
 import game.utils.RandomNumberGenerator;
+import org.w3c.dom.ranges.Range;
 
 public class GodrickSoldier extends Enemy {
 
     public GodrickSoldier() {
         super("Godrick Soldier", 'p', 198);
-        //this.addWeaponToInventory(new HeavyCrossbow());
+        this.addWeaponToInventory(new HeavyCrossbow());
         this.addCapability(EnemyFamily.CASTLE_DWELLER);
+        behaviours.put(4, new RangeBehaviour());
+
     }
+
 
     @Override
     public int generateRunes(){
