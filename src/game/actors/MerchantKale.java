e152c5d14161dd149d89ad15d58ed88b4d597de4
salar <sgha0017@student.monash.edu>
2023-05-18 20:11:24 +1000

    abstracted Merchant and refactored MerchantKale

--- src/game/actors/MerchantKale.java
+++ src/game/actors/MerchantKale.java
@@ -1,50 +1,18 @@
 package game.actors;
 
-import edu.monash.fit2099.engine.actions.Action;
-import edu.monash.fit2099.engine.actions.ActionList;
-import edu.monash.fit2099.engine.actions.DoNothingAction;
-import edu.monash.fit2099.engine.actors.Actor;
-import edu.monash.fit2099.engine.displays.Display;
-import edu.monash.fit2099.engine.positions.GameMap;
-import game.actions.SellAction;
-import game.actions.Buyable;
-import game.actions.BuyAction;
 import game.items.weapons.Club;
 import game.items.weapons.GreatKnife;
 import game.items.weapons.HeavyCrossbow;
 import game.items.weapons.Uchigatana;
 
-import java.util.ArrayList;
 
-public class MerchantKale extends Actor {
+public class MerchantKale extends Merchant{
 
-    private ArrayList<Buyable> valuables = new ArrayList<>();
     public MerchantKale(){
         super("Merchant Kale",'K',100);
-        valuables.add(new Club());
-        valuables.add(new Uchigatana());
-        valuables.add(new GreatKnife());
-        valuables.add(new HeavyCrossbow());
-    }
-
-
-    @Override
-    public Action playTurn(ActionList actions, Action lastAction, GameMap map, Display display) {
-        return new DoNothingAction();
-    }
-
-    @Override
-    public ActionList allowableActions(Actor otherActor, String direction, GameMap map) {
-        return playerAllowableActions((Player) otherActor);
-    }
-    public ActionList playerAllowableActions(Player player) {
-        ActionList actions = new ActionList();
-        for (Buyable buyable: player.getValuables()){
-            actions.add(new SellAction(this, player, buyable));
-        }
-        for (Buyable buyable: valuables){
-            actions.add(new BuyAction(this, player, buyable));
-        }
-        return actions;
+        addValuableToInventory(new Club());
+        addValuableToInventory(new Uchigatana());
+        addValuableToInventory(new GreatKnife());
+        addValuableToInventory(new HeavyCrossbow());
     }
 }
